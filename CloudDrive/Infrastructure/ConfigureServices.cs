3bd493eb9e85705245f72a17495367b677b3463a
Kirill Sannikov <kir.sannikow@mail.ru>
2023-10-07 20:46:54 +0300

    Инициализация базовой архитектуры

---
+++ CloudDrive/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Foundations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Application.Foundations;
+using Infrastructure.Database;
+
+namespace Infrastructure;
+
+public static class ConfigureServices
+{
+    public static void AddDatabaseFoundations(this IServiceCollection services, IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("LocalPostgreSQL");
+
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+}
